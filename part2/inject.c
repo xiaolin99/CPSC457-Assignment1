006544d60b6ddc104607194478558348335aa563
xiaolin99 <xiao.lin99@gmail.com>
2014-10-03 11:36:28 -0600

    p2 wip

--- part2/inject.c
+++ part2/inject.c
@@ -11,41 +11,77 @@
 #include <asm/ptrace.h>
 #include <udis86.h>
 
+// some offsets in ELF
 #define L_ENDIAN 0x01 // LSB to the right
 #define B_ENDIAN 0x10 // LSB to the left
 #define LOC_ENDIAN 0x5 // Endianness 1 byte
+#define LOC_ENTRY 0x18 // Execution entry 4 bytes (mem location for _start)
+#define LEN_LOC_ENTRY 4
 #define LOC_PH 0x1C // Program header start 4 bytes
+#define LEN_LOC_PH 4
 #define LOC_SH 0x20 // Section header start 4 bytes
+#define LEN_LOC_SH 4
 #define SIZE_PH 0x2A // Program header size 2 bytes
-#define SIZE_SH 0x2E // Section header size 2 bytes
+#define LEN_SIZE_PH 2
 #define NUM_PH 0x2C // num entries in Program header 2 bytes
+#define LEN_NUM_PH 2
+#define SIZE_SH 0x2E // Section header size 2 bytes
+#define LEN_SIZE_SH 2
 #define NUM_SH 0x30 // num entries in Section header 2 bytes
+#define LEN_NUM_SH 2
 #define IDX_SNAME 0x32 // index of section header entry that contain section names 2 bytes 
+#define LEN_IDX_SNAME 2
 
 
 
 int main(int argc, char* argv[])
 {
   FILE *target;
-  target = fopen("./a.out", "a+");
+  target = fopen(argv[1], "a+");
   if (!target) {
     printf("unable to open file");
     return 1;
   }
-  unsigned char buff;
+  long buff = 0;
   unsigned long location = 0;
   unsigned long target_size = 0; 
   fseek (target , 0 , SEEK_END);
   target_size = ftell (target);
   rewind (target);
-  printf("target size: %ld Bytes\n", target_size);
+  
   int i;
-  for (i = 0; i < target_size; i ++) {
+  for (i = 0; i*4 < target_size; i ++) {
     location = ftell(target);
     printf("%08lx: ", location);
-    fread(&buff, 1, 1, target);
-    printf("%02x\n", (unsigned int)buff);
+    buff = 0;
+    fread(&buff, 1, 4, target);
+    printf("%08lx  ", buff);
+    unsigned char *str = (unsigned char*)&buff;
+    printf("%s\n", str); 
   }
+  printf("target size: %ld Bytes\n", target_size);
+
+  buff = 0;
+  fseek(target, LOC_ENTRY, 0);
+  fread(&buff, 1, LEN_LOC_ENTRY, target);
+  printf("ENTRY = %08lx\n", buff);
+
+  buff = 0;
+  fseek(target, LOC_PH, 0);
+  fread(&buff, 1, LEN_LOC_PH, target);
+  printf("PH offset = %08lx\n", buff);
+
+  buff = 0;
+  fseek(target, LOC_SH, 0);
+  fread(&buff, 1, LEN_LOC_SH, target);
+  printf("SH offset = %08lx\n", buff);
+  fseek(target, buff, 0);
+
+
+  buff = 0;
+  fseek(target, IDX_SNAME, 0);
+  fread(&buff, 1, LEN_IDX_SNAME, target);
+  printf("Index of SH name = %08lx\n", buff);
 
   fclose(target);
 
---
+++ part2/test.asm
@@ -0,0 +1,15 @@
+section .text
+global _start
+_start:
+loop_start:
+	xor eax, eax
+	xor ebx, ebx
+	inc ebx
+	inc eax
+	nop
+	nop
+	add eax, 0x10
+	sub eax, 0x10
+	imul eax, ebx, 0x2
+	neg ebx
+	jmp loop_start 
